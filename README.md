cb9e47445a005fd206adb26be11af14aee4b64e5
Sergei Krivosheenko <serkyron@gmail.com>
2019-10-18 13:58:11 +0300

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+  # MongoDB log channel for composite logger  
+  Use this package in your applications for logging to MongoDB.
+  You can learn more about composite logger [here](https://www.npmjs.com/package/ts-composite-logger).  
+    
+  **npm install -S ts-mongodb-logger**  
+    
+  ## Usage  
+    
+  	import {Logger} from "ts-composite-logger";
+  	import {MongoDB} from "ts-mongodb-logger";
+      
+  	const mongo = new MongoDB();
+  	mongo.connect(
+	  "mongodb://serkyron:password@localhost:27017/?authMechanism=SCRAM-SHA-1&authSource=dbname",
+	  "dbname",
+	  "log"
+	)
+  	  .catch((e) => {console.error(e.message)});
+  	
+  	global.logger = (new Logger())
+	  .addChannel(mongo);
+  		
+  	global.logger.info("VM connected");
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import assert from "assert";
+import {MongoClient} from "mongodb";
+import {ILoggerChannel} from "ts-composite-logger";
+import {ILogMessage} from "ts-composite-logger";
+
+export class MongoDB implements ILoggerChannel {
+    private readonly connectUrl: string;
+    private readonly dbName: string;
+    private readonly collectionName: string;
+    private client;
+
+    constructor(url: string, db: string, collection: string) {
+        this.connectUrl = url;
+        this.dbName = db;
+        this.collectionName = collection;
+    }
+
+    public async write(message: ILogMessage) {
+        if (!this.client) {
+            throw new Error("Channel not connected to DB");
+        }
+
+        const db = this.client.db(this.dbName);
+        const collection = db.collection(this.collectionName);
+        await this.insert(collection, message);
+    }
+
+    private insert(collection, message: ILogMessage) {
+        return new Promise((resolve, reject) => {
+            collection.insertOne(message, (error, result) => {
+                if (error) {
+                    return reject(error);
+                }
+
+                return resolve();
+            });
+        });
+    }
+
+    public connect(): Promise<ILoggerChannel> {
+        return new Promise((resolve, reject) => {
+            MongoClient.connect(this.connectUrl, { useNewUrlParser: true }, (err, client) => {
+                try {
+                    assert.strictEqual(null, err);
+                } catch (e) {
+                    return reject(e);
+                }
+
+                this.client = client;
+                resolve(this);
+            });
+        });
+    }
+}
